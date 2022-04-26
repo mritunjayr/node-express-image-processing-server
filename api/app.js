8e866ebfc7f82d117401d54e7dee9612bab84b77
Mritunjay Kumar <mritunjaykumar694@gmail.com>
2022-04-26 12:58:34 +0530

    module 2 completed

--- api/app.js
+++ api/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const path = require("path");
+const { apply } = require("ramda");
 
 const app = express();
 const pathToIndex = path.resolve(__dirname, "../client/index.html");
+
+const router = require("./src/router");
+app.use("/", router);
+app.use(express.static(path.resolve(__dirname, "uploads")));
 app.use("/*", (request, response) => {
   response.sendFile(pathToIndex);
 });
